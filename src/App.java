8f453e200c34d2cd967f0eb3da27e59472286b2a
Jonah <jonahrabinovitch11@gmail.com>
2025-10-21 10:15:46 -0400

    Thanks!

--- src/App.java
+++ src/App.java
@@ -4,12 +4,14 @@ public class App extends PApplet{
     public static void main(String[] args)  {
         PApplet.main("App");
     }
-    float rectX = 0;
-    float rectY = 0;
-    float speed = 5;
+    int rectX = 0;
+    int rectY = 0;
+    int speed = 5;
     int score = 0;
     float foodX = random(0, 400);
     float foodY = random(0, 400);
+    boolean left, right;
+    boolean up, down;
     public void setup(){
         background(0, 0, 80);
     }
@@ -23,24 +25,52 @@ public class App extends PApplet{
         background(0, 0, 80);
         rect(rectX, rectY, 50, 50);
         rect(foodX, foodY, 50, 50);
+        if (left == true){
+            rectX -= speed;
+        }
+        if (right == true){
+            rectX += speed;
+        }
+        if (down == true){
+            rectY += speed;
+        }
+        if (up == true){
+            rectY -= speed;
+        }
+        score();
     }
     public void keyPressed(){
-        if (key == 'd'){ // error corerected w/ chatGPT
-        rectX = rectX + speed;
-        }
         if (key == 'w'){
-            rectY = rectY - speed;
-        }
-        if (key == 'a'){
-            rectX = rectX - speed;
+            left = false;
+            right = false;
+            down = false;
+            up = true;
         }
         if (key == 's'){
-            rectY = rectY + speed;
+            left = false;
+            up = false;
+            right = false;
+            down = true;
+        }
+        if (key == 'd'){
+            left = false;
+            up = false;
+            down = false;
+            right = true;
+        }
+        if (key == 'a'){
+            up = false;
+            down = false;
+            right = false;
+            left = true;
         }
     }
     public void score(){
-        if (rectX == foodX || rectY == foodY){
-            score++;
+        if (rectX - foodX >= -50 && rectX - foodX <=0 && rectY - foodY >= -50 && rectY - foodY <= 0){
+            score = score + 1;
+            System.out.println(score);
+            background(0, 0, 80);
+            
         }
     }
 }
