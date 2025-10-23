4cee02cfc145f5fbd1679b295cee6d74c6320e18
Jonah <jonahrabinovitch11@gmail.com>
2025-10-23 15:54:15 -0400

    How can I add a picture to the rectangles?

--- src/App.java
+++ src/App.java
@@ -7,13 +7,15 @@ public class App extends PApplet {
 
     int rectX = 100;
     int rectY = 100;
-    int speed = 5;
+    double speed = 5;
     int score = 0;
     float foodX = random(0, 400);
     float foodY = random(0, 400);
     boolean left, right;
     boolean up, down;
     int scene = 1;
+    float foodSpeedX = 3;
+    float foodSpeedY = 3;
 
     public void setup() {
         background(0, 0, 80);
@@ -46,18 +48,22 @@ public class App extends PApplet {
             System.out.println("Game over");
             scene = 3;
         }
-        if (scene == 1){
+        if (scene == 1) {
             background(0, 0, 80);
-            text("Welcome!", 100, 100);
+            text("Welcome to Be a Job Application!", 100, 100);
         }
-        if (scene == 3){
+        if (scene == 3) {
             background(0, 0, 80);
-            text("Game over", 100, 100);
+            text("Game over", 500, 100);
         }
+        if (scene == 2 || scene == 3) {
+            text("Score: " + score, 100, 100);
+        }
+        runAway();
     }
 
     public void keyPressed() {
-        if (key == ' '){
+        if (key == ' ') {
             scene = 2;
         }
         if (key == 'w') {
@@ -93,6 +99,7 @@ public class App extends PApplet {
             background(0, 0, 80);
             foodX = random(550);
             foodY = random(550);
+            speed += .1;
         }
     }
 
@@ -103,5 +110,18 @@ public class App extends PApplet {
         } else {
             return false;
         }
+    }
+
+    public void runAway() {
+        if (frameCount % 50 == 0) {
+            foodSpeedX = random(-3, 3);
+            foodSpeedY = random(-3, 3);
+        }
+        if (foodX + 50 > 600 || foodX < 0 || foodY + 50 > 600 || foodY < 0) {
+            foodSpeedX = -foodSpeedX;
+            foodSpeedY = -foodSpeedY;
+        }
+        foodX += foodSpeedX;
+        foodY += foodSpeedY;
     }
 }
